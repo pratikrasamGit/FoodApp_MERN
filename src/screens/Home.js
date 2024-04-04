06687700a0b71bde0a601f38854857b4c71c042b
pratikrasamGit <158097151+pratikrasamGit@users.noreply.github.com>
2024-04-05 03:59:06 +0530

    fooddata err

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,8 +9,8 @@ export default function Home() {
   const [foodCat, setFoodCat] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
 
-  const loadData = async () => {
-    let response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/foodData`, {
+  const loadData =  () => {
+    let response =  fetch(`${process.env.REACT_APP_BASE_URL}/api/foodData`, {
       mode: 'no-cors',
       method: "POST",
       headers: {
@@ -20,7 +20,7 @@ export default function Home() {
       }
     });
     console.log(response.json());
-    let fdata = await response.json();
+    let fdata =  response.json();
     console.log(fdata[0]);
     setFoodCat(fdata[1]);
     setFoodItem(fdata[0]);
