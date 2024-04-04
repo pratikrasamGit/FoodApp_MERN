f7d9758706463bf566898d2bfe5a057b5d4e6e35
pratikrasamGit <158097151+pratikrasamGit@users.noreply.github.com>
2024-04-05 03:06:34 +0530

    fooddata err

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -15,7 +15,7 @@ export default function Home() {
       method: "POST",
       headers: {
         "Accept" : "application/json",
-        // "Content-Type": "application/json",
+        "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
       }
     });
